33c2378ecdaff18d692fc9091cd2ff90b42261a6
Bang Nguyen <bangnv77@gmail.com>
2016-08-08 23:58:45 -0700

    review codes, display as expected, end of part 3 of the Tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,27 +3,42 @@ export class Hero {
   id: number;
   name: string;
 }
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
     <div *ngIf="selectedHero">
       <h2>{{selectedHero.name}} details!</h2>
       <div><label>id: </label>{{selectedHero.id}}</div>
       <div>
         <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name" />
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
       </div>
     </div>
-    <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-    `,
+  `,
   styles: [`
     .selected {
-      background-color: #cfd8dc !important;
+      background-color: #CFD8DC !important;
       color: white;
     }
     .heroes {
@@ -36,57 +51,44 @@ export class Hero {
       cursor: pointer;
       position: relative;
       left: 0;
-      background-color: #eee;
+      background-color: #EEE;
       margin: .5em;
       padding: .3em 0;
       height: 1.6em;
       border-radius: 4px;
     }
     .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-  `],
-  [class.selected]="hero === selectedHero"
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  selectedHero: Heroes;
+  heroes = HEROES;
+  selectedHero: Hero;
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice'},
-  { id: 12, name: 'Mr. Narco'},
-  { id: 13, name: 'Mr. Bombasto'},
-  { id: 14, name: 'Mr. Celeritas'},
-  { id: 15, name: 'Mr. Magneta'},
-  { id: 16, name: 'Mr. RubberMan'},
-  { id: 17, name: 'Mr. Dynama'},
-  { id: 18, name: 'Mr. Dr IQ'},
-  { id: 19, name: 'Mr. Magma'},
-  { id: 20, name: 'Mr. Tornado'},
-];
